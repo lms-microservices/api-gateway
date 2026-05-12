541e7076cef6b39ddd6de4770af8214fdb8e0e31
DORMODO <ymb01072005@gmail.com>
2026-05-12 08:29:48 +0300

    feat: add AWS profile, update Dockerfile and gateway config

--- src/main/java/com/lms/api_gateway/config/WebClientConfig.java
+++ src/main/java/com/lms/api_gateway/config/WebClientConfig.java
@@ -1,19 +1,29 @@
 package com.lms.api_gateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerExchangeFilterFunction;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
+    @Profile("!aws")
     public WebClient authWebClient(ReactorLoadBalancerExchangeFilterFunction lbFunction) {
         return WebClient.builder()
                 .filter(lbFunction)
                 .baseUrl("http://auth-service")
                 .build();
+    }
 
+    @Bean
+    @Profile("aws")
+    public WebClient authWebClientDirect(@Value("${auth.service.url:http://auth-service:8086}") String authServiceUrl) {
+        return WebClient.builder()
+                .baseUrl(authServiceUrl)
+                .build();
     }
 }
